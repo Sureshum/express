561eec3ea91a53472a3712a9d08edd4905ac81c7
Sebastián Shum <89267654+Sureshum@users.noreply.github.com>
2025-04-26 17:49:14 +0100

    Delete expressdesafio4 directory

--- expressdesafio4/src/routes/dashboardRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { verifyToken } = require('../middlewares/authJwt');
-const { getDashboard } = require('../controladores/dashboardController');
-
-router.get('/', verifyToken, getDashboard);
-
-module.exports = router;
--- expressdesafio4/src/routes/detailRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { verifyToken } = require('../middlewares/authJwt');
-const { getDetail } = require('../controladores/detailController');
-
-router.get('/:id', verifyToken, getDetail);
-
-module.exports = router;
